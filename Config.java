3e8c38fb7c2e3d0d4dd283d8f5121e72753ce5a0
= <=>
2019-03-02 10:01:22 +0800

    update in 2019-03-02

--- Config.java
+++ Config.java
@@ -2,6 +2,8 @@ package com.zbyj.Yazhou.LeftCompanyProgram;
 
 public class Config {
 
+    public static final String DEBUG = "LeftCompanyProgram";
+
     /**
      * http访问的数据类型
      */
@@ -12,9 +14,9 @@ public class Config {
 
 
     /**
-     *获取返回数据的表示符
+     * 获取返回数据的表示符
      */
-    public static class HttpMethodRequestStatus{
+    public static class HttpMethodRequestStatus {
         public static final String HTTP_REQUEST_STATUS = "status";//返回的状态
     }
 
@@ -22,21 +24,51 @@ public class Config {
     /**
      * 注册用户的登录返回的状态信息
      */
-    public static class LRUserRequestStatus{
+    public static class LRUserRequestStatus {
         public static final String LRUSER_OK = "0";//登录成功
         public static final String LRUSER_ERROR = "1";//登录失败 限制登录
         public static final String LRUSER_TOMUCH_ERROR = "2";//登录失败 验证码错误次数太多
         public static final String LRUSER_ERRORCODE = "3";//登录失败 验证码错误
     }
 
-
     /**
      * 返回的登录Json数据中 对应的用户的字段
      */
-    public static class JSON_USERPAGE{
+    public static class JSON_USERPAGE {
         public static final String USER_NAME = "name";//姓名
         public static final String USER_PHNOE = "phone";//电话
         public static final String USER_PHONE_MD5 = "phone_md5";//用户的加密MD5
+        public static final String USER_EROR_NUMBER = "err_number";//用户登录错误次数
+        public static final String USER_TOKEN = "token";//验证秘钥
+        public static final String USER_SEX = "sex";//用户的性别
+        public static final String USER_BIRTHDATE = "birthdate";//生日 即出生日期
+        public static final String USER_CARDID = "carid";//身份证号码
+        public static final String USER_CARDNAME = "carname";//身份证名称
+        public static final String USER_CARDSTART = "cardstart";//身份证开始的时间
+        public static final String USER_CARDEND = "cardend";//身份证失效时间
+        public static final String USER_CARDIN = "cardin";//身份证所属的省份
+        public static final String USER_LEFTCOMPANY_ID = "leftcompany_id";//用户在左边远景软件开发工作室的唯一ID
+        public static final String USRE_LOGIN_VERIFICATION = "login_verifction";//登录验证码
+        public static final String USER_REG_TIME = "reg_time";//用户注册的时间
     }
+
+
+    /**
+     * 返回所需要服务的地址
+     */
+    public static class HTTP_ADDR {
+        public static String SERVICE = "http://47.102.205.26";
+
+
+        /**
+         * 获取发送短信验证码的接口地址
+         *
+         * @return
+         */
+        public static String reSendVerificationCodeAddr() {
+            return HTTP_ADDR.SERVICE + "";
+        }
+    }
+
 
 }
--- Net.java
+++ Net.java
@@ -1,14 +1,112 @@
 package com.zbyj.Yazhou.LeftCompanyProgram;
 
 
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+
+import com.zbyj.Yazhou.tools;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * 网络访问模块  有关于网络访问的监听和提交事件都在这里
- *
+ * <p>
  * 该模块属于左边远景软件开发工作室公司服务器内模块 任何开发个体的android project都可以使用
- * 该套模块,如果您要修改源代码 您应该考虑该份代码中不能存在任何非公司服务器中不存在的模块引用
- * 因为这样会带来很多未知的OOM
+ * 该套模块
  */
 public class Net {
+    private String tUrl = "";
+    private StringBuffer mKvsBuffer = new StringBuffer();
+    private Net.onVisitInterServiceListener mOnVisitInterServiceListener;
+    private int VisitInterMethod;
+
+    /**
+     * 用GET方式获取数据信息
+     *
+     * @param tUrl                         地址
+     * @param mOnVisitInterServiceListener 监听回调
+     * @param kvs                          参数对,没有就直接用NULL
+     */
+    public static void InterServiceGet(Context context, String tUrl, final Net.onVisitInterServiceListener mOnVisitInterServiceListener, String... kvs) {
+        if (!Tools.isIntentConnect(context)) {
+            //网络无连接 就不做什么操作了
+            if (mOnVisitInterServiceListener != null) {
+                mOnVisitInterServiceListener.onNotConnect();
+            }
+            return;
+        } else {
+            final StringBuffer kvsBuffer = new StringBuffer();
+            if (kvs != null && kvs.length > 1) {
+                try {
+                    for (int i = 0; i < kvs.length; i += 2) {
+                        kvsBuffer.append(kvs[i] + "=" + kvs[i + 1] + "&");
+                    }
+                } catch (Exception e) {
+                    Log.e(Config.DEBUG, "SystemVisitInerService.java[+]:" + e.getMessage());
+                }
+            }
+            new AsyncTask<String, Void, String>() {
+                @Override
+                protected String doInBackground(String... urls) {
+                    try {
+                        HttpURLConnection con = (HttpURLConnection) new URL(urls[0].trim().toString() + "?" + kvsBuffer.toString()).openConnection();
+                        Log.i(Config.DEBUG, "访问网络地址:" + urls[0].trim().toString() + "?" + kvsBuffer.toString());
+                        con.setRequestMethod("GET");
+                        con.setConnectTimeout(5000);
+                        con.setReadTimeout(5000);
+                        con.connect();
+                        if (con != null) {
+                            InputStream is = con.getInputStream();
+                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+                            StringBuffer sb = new StringBuffer();
+                            String ReadLine = "";
+                            while ((ReadLine = bufferedReader.readLine()) != null) {
+                                sb.append(ReadLine);
+                            }
+                            is.close();
+                            con.disconnect();
+                            bufferedReader.close();
+                            return sb.toString();
+                        } else {
+                            return null;
+                        }
+                    } catch (Exception e) {
+                        Log.e(Config.DEBUG, "SystemVisitInerService.java[+]:" + e.getMessage());
+
+                    }
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+                    if (s != null) {
+                        if (mOnVisitInterServiceListener != null) {
+                            mOnVisitInterServiceListener.onSucess(s);
+                        }
+                    } else {
+                        if (mOnVisitInterServiceListener != null) {
+                            mOnVisitInterServiceListener.onFail("");
+                        }
+                    }
+                    super.onPostExecute(s);
+                }
+            }.execute(tUrl);
+        }
+    }
 
 
+    public interface onVisitInterServiceListener {
+
+        void onSucess(String tOrgin);//成功的监听
+
+        void onNotConnect();//网络断开连接
+
+        void onFail(String tOrgin);//失败的监听
+    }
 }
